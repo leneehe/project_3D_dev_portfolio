aaa23d5fc4b1e7ee08d8f1673320dc10118c05d9
leneehe <qy.lena@gmail.com>
2024-11-14 02:57:15 -0500

    add avatar portrait to hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,24 +1,36 @@
 "use client";
 import { motion } from "framer-motion";
 import React from "react";
+import Image from "next/image";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
 	return (
 		<section className="relative w-full h-screen mx-auto">
-			<div className="paddingX absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5">
-				<div className="flex flex-col justify-center items-center mt-5">
-					<div className="w-5 h-5 rounded-full bg-[#915EFF] " />
-					<div className="w-1 sm:h-80 h-40 violet-gradient" />
+			<div className="paddingX absolute inset-0 top-[120px] max-w-7xl flex flex-row">
+				<div className="ml-auto min-w-40 pt-6 md:pt-2 px-6 md:px-8">
+					<Image
+						src="/avatar.jpg"
+						className="rounded-full border-4 md:border-8 border-indigo-950"
+						width={200}
+						height={200}
+						alt="Lena He"
+						/>
 				</div>
-				<div>
-					<h1 className="heroHeadText text-white">
-						Hi, I&apos;m <span className="text-[#915EFF] ">Lena</span>
-					</h1>
-					<p className="heroSubText">
-						A Software Engineer, Frontend Developer&nbsp;
-						<br className="sm:block hidden" />& UI/UX Designer
-					</p>
+				<div className="flex flex-row items-start gap-5">
+					<div className="flex flex-col justify-center items-center mt-5">
+						<div className="w-5 h-5 rounded-full bg-[#915EFF] " />
+						<div className="w-1 sm:h-80 h-40 violet-gradient" />
+					</div>
+					<div>
+						<h1 className="heroHeadText text-white">
+							Hi, I&apos;m <span className="text-[#915EFF] ">Lena</span>
+						</h1>
+						<p className="heroSubText">
+							A Software Engineer, Frontend Developer&nbsp;
+							<br className="sm:block hidden" />& UI/UX Designer
+						</p>
+					</div>
 				</div>
 			</div>
 			<ComputersCanvas />
